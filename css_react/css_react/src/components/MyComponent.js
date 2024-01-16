db057b288f7caf20064247676ba7612c1e514b52
Keila <keilacris2001clr@gmail.com>
2024-01-15 23:41:50 -0300

    CSS

---
+++ css_react/css_react/src/components/MyComponent.js
@@ -0,0 +1,13 @@
+import "./MyComponent.css"
+
+const MyComponent = () => {
+  return (
+    <div>
+        <h1>CSS de componente</h1>
+        <p>Este é um parágrafo do componente</p>
+        <p className="my-comp-paragrafo">Este também é do componente</p>
+    </div>
+  )
+};
+
+export default MyComponent
